5c5cec5dcf0f7d16a78ae2560ec68909a8ebdc1f
Ashish Garg <mragarg@gmail.com>
2019-03-12 14:41:42 -0400

    Styles page, first check on fetch API URL

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
 const API_URL = 'https://dc-coffeerun.herokuapp.com/api/coffeeOrders';
+
+fetch(API_URL)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    });
