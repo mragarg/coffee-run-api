3b4c94111f2e246744d816e5013f2298893d6311
Ashish Garg <mragarg@gmail.com>
2019-03-13 09:21:25 -0400

    Adds coffee.json file due to API URL data being deleted

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
 const API_URL = 'https://dc-coffeerun.herokuapp.com/api/coffeeOrders';
 
-fetch(API_URL)
-    .then(function (response){
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data);
-    });
+let orderDetailsArray = [];
+
+function getEmail(url){
+    fetch(API_URL)
+        .then(function (response){
+            return response.json();
+        })
+        .then(function (data){
+            console.log(data);
+            // getCoffeeData(data);
+        });
+}
+
+function getCoffeeData(data) {
+    orderDetailsArray = [
+        ...orderDetailsArray,
+        ...data
+    ];
+}
+
+
+getEmail(API_URL);
